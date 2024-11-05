16fcef412cefce28b64850c6581d0b2f02c17309
onur.akmese <onur.akmese@lhs-sa.ch>
2024-11-05 12:56:50 +0100

    Initialized the project

---
+++ csharp-net-maui-pizza-world/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace csharp_net_maui_pizza_world
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ csharp-net-maui-pizza-world/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_net_maui_pizza_world
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
